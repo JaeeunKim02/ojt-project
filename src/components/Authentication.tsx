9f274c2667680a33106c08b0b67e96035872b160
kimjaeeun <kimjaeeun@jaeeunkim-macos.local>
2024-03-11 11:33:11 +0900

    화면 깜빡임은 없어짐..

--- src/components/Authentication.tsx
+++ src/components/Authentication.tsx
@@ -9,21 +9,25 @@ function Authentication(linkTo1:string, linkTo2:string) {
     // [x]: api를 불러와서 athorization 통과해야 마이페이지 보여주기 /islogin 조작가능하므로...
     useEffect(() => { //[x] 마운트 될때만 하는거면 useEffect 쓸필요 있나?->리렌더링하는게 너무 많다고 뜸..useEffect 하면 안뜸
       // 로컬 스토리지에서 accessToken 존재 여부 확인
-      const checkAuthentication = () =>{
+      const checkAuthentication = async () =>{
         const accessToken = localStorage.getItem('accessToken');
         const userId=localStorage.getItem('id');
         try{
-          fetch(`https://levelzero-backend.platform-dev.bagelgames.com/user/info/${userId}`,{
+          const response = await fetch(`https://levelzero-backend.platform-dev.bagelgames.com/user/info/${userId}`,{
             method: 'GET',
             headers: {
               Authorization: `Bearer ${accessToken}`
               },
             // cache: 'no-store',
           })
-          router.push('/mypage')
-        } catch(error){
+
+          if (!response.ok)
+            router.push(linkTo2)  
+          else
+            router.push(linkTo1)
+        } catch(error) {
           console.error(error);
-          router.push('/auth/login')
+          router.push(linkTo2)
         }
       };
       checkAuthentication();
