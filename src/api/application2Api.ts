e58b6e3925a552d5298c7dc40525d655a236093f
kimjaeeun <kimjaeeun@jaeeunkim-macos.local>
2024-03-29 12:18:10 +0900

    fix updateModal

--- src/api/application2Api.ts
+++ src/api/application2Api.ts
@@ -61,5 +61,5 @@ export async function createApplication(
       message: `${error}` || 'An error occurred during adding application.',
     };
   }
-  redirect('/application?page=1&size=10'); //try-catch 문에서 사용은 자제하기, try 안에서 redirect 하면 redirect가 내부적으로 error로 인식해버림!
+  redirect('/application2?page=1&size=12'); //try-catch 문에서 사용은 자제하기, try 안에서 redirect 하면 redirect가 내부적으로 error로 인식해버림!
 }
--- src/app/application/@AppModal/update/[id]/page.tsx
+++ src/app/application/@AppModal/update/[id]/page.tsx
@@ -6,6 +6,8 @@ import Typography from '@mui/material/Typography';
 import { Button, TextField } from '@mui/material';
 import { useFormState } from 'react-dom';
 import { updateModal } from '../../../../../api/applicationApi';
+import { Cookies } from 'react-cookie';
+
 //[x] go to app(페이지로 라우팅) 한 뒤에 update 할 수 있게, 기존의 내용이 보이는 상태이어야 함.
 //[x] 뒤로가기, 홈 버튼 항상 보일 수 있게
 const style = {
@@ -22,60 +24,81 @@ const style = {
   flexDirection: 'column',
 };
 
-export default function Page({ params }: { params: { id: string } }) {
+export default async function Page({ params }: { params: { id: string } }) {
   const router = useRouter();
   const [state, action] = useFormState(updateModal, { message: '' });
-  console.log(params.id);
-  return (
-    <Modal
-      open={true}
-      onClose={() => {
-        router.back();
-      }}
-      aria-labelledby="modal-modal-title"
-      aria-describedby="modal-modal-description"
-    >
-      <Box sx={style}>
-        <Typography id="modal-modal-title" variant="h6" component="h2">
-          Update your application
-        </Typography>
-        {/* [ ] */}
-        <Typography
-          id="modal-modal-description"
-          className="mt-[5px] text-[rgb(150,150,150)] text-[15.5px]"
+  const cookies = new Cookies();
+  const accessToken = cookies.get('accessToken');
+  try {
+    const res = await fetch(`${process.env.API_URL}/application/${params.id}`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    if (!res.ok) {
+      if (res.status === 404) throw new Error('Application not found');
+      throw new Error('Authentication error');
+    } else {
+      const dto = await res.json();
+      console.log(dto.id, dto.name, dto.description);
+      console.log(params.id);
+      return (
+        <Modal
+          open={true}
+          onClose={() => {
+            router.back();
+          }}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
         >
-          * name, description is required *
-        </Typography>
-        <form
-          action={action}
-          className="flex items-center flex-col gap-[10px] mt-[20px]"
-        >
-          <input type="hidden" name="id" id="id" value={params.id}></input>
-          <TextField
-            required
-            id="name"
-            label="name"
-            variant="outlined"
-            name="name"
-          />
-          <TextField
-            required
-            id="description"
-            label="description"
-            variant="outlined"
-            name="description"
-          />
-          {/* [ ] */}
-          <Button
-            type="submit"
-            variant="contained"
-            className="bg-[#1976d2] text-[#fff] mt-[10px]"
-          >
-            Update Application
-          </Button>
-          <p className="text-red-500">{state.message}</p>
-        </form>
-      </Box>
-    </Modal>
-  );
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Update your application
+            </Typography>
+            {/* [ ] */}
+            <Typography
+              id="modal-modal-description"
+              className="mt-[5px] text-[rgb(150,150,150)] text-[15.5px]"
+            >
+              * name, description is required *
+            </Typography>
+            <form
+              action={action}
+              className="flex items-center flex-col gap-[10px] mt-[20px]"
+            >
+              <input type="hidden" name="id" id="id" value={params.id}></input>
+              <TextField
+                required
+                id="name"
+                label="name"
+                variant="outlined"
+                name="name"
+                defaultValue={dto.name}
+              />
+              <TextField
+                required
+                id="description"
+                label="description"
+                variant="outlined"
+                name="description"
+                defaultValue={dto.description}
+              />
+              {/* [ ] */}
+              <Button
+                type="submit"
+                variant="contained"
+                className="bg-[#1976d2] text-[#fff] mt-[10px]"
+              >
+                Update Application
+              </Button>
+              <p className="text-red-500">{state.message}</p>
+            </form>
+          </Box>
+        </Modal>
+      );
+    }
+  } catch (error) {}
+  console.error('Authentication error');
+  return <>Sorry... Application not found.</>;
 }
